b0c6088e48b9e62996ca809e45ddb2fa6555d43e
[Kim-Hyeong-Jong] <[khj9899@ajou.ac.kr]>
2018-06-11 20:33:28 +0900

    구글맵 API 적용 기본 위치 서울시 적용 MapView사용(xml에서)

--- app/src/main/java/bangbanggokgok/com/com/com/mobile_project/MapFragment.java
+++ app/src/main/java/bangbanggokgok/com/com/com/mobile_project/MapFragment.java
@@ -9,24 +9,103 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
  * Created by User on 2018-06-07.
  */
 
-public class MapFragment extends Fragment  {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    private Activity root;
+
+    private MapView mapView = null;
+
+
+    public MapFragment() {
+        //
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.googlemap,container,false);
-        root = getActivity();
+
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.getMapAsync(this);
 
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//액티비티가 처음 생성될 때 실행되는 함수
+
+        if(mapView != null)
+        {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng Seoul = new LatLng(37.56, 126.97);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Seoul));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+    }
 }
